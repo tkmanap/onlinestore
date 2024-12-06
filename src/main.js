bdfe24f53e0b5695a6321b7ddc64aff87fddc1fb
manaptk <tk.manap@gmail.com>
2024-12-06 20:10:15 +0500

    2.1 Vue-router

--- src/main.js
+++
@@ -1,7 +0,0 @@
-import './assets/style/main.css'
-
-import { createApp } from 'vue'
-import App from './App.vue'
-import {router} from "@/router/router.js";
-
-createApp(App).use(router).mount('#app')
